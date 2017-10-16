1fcc61be856a83173e672a85444f5ccb4b2566ad
Rai <rai@ingmechs.com>
2017-10-16 17:53:01 +0200

    Added new map

--- upload_gas.sh
+++ upload_gas.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd '/Volumes/Macintosh HD/_GitHub/gas'
-rsync -avzh pi@192.168.1.135:/media/pi/RAI/logs/gasolineras/ /Volumes/MacintoshHD/_data/gasolineras/
+cd '/Volumes/MacintoshHD/_GitHub/gas'
+rsync -avzh pi@192.168.1.135:/media/pi/RAI/logs/gasolineras/ /Volumes/MacintoshHD/_data/gasolineras/ -p 'J4v5f7o3'
 python generate_map.py
 git add *
 git commit -m "Added new map"
